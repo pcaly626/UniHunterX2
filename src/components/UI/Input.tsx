db6d667722a65435cbdfc9ffd50b23c7007c7efd
Vince <vparish13@gmail.com>
2021-01-19 12:17:38 -0600

    experimental changes to be saved

---
+++ src/components/UI/Input.tsx
@@ -0,0 +1,46 @@
+import React, {Fragment} from 'react';
+
+const inputElement ={
+    label:"",
+    placeHolder:"",
+    value:"",
+    class:"",
+    type: "",
+    change: function(event: any) {},
+    options: [
+        {
+            value:"",
+            class:"",
+        }
+    ]
+}
+type InputElement = typeof inputElement;
+
+const input = (props : InputElement) =>{
+    let inputElement;
+    switch(props.type) {
+        case 'input': inputElement = (<input className={props.class} value={props.value} placeholder={props.placeHolder} onChange={props.change}/>)
+            break;
+        case 'select':inputElement =
+        <select className={props.class} onChange={props.change}>
+            {props.options.map( option => (
+                <option {...option}>
+                    {option.value}
+                </option>
+            ))}
+        </select>
+        break;
+        case 'checkbox': inputElement = ("HTML here like above")
+        
+        default: inputElement = <input/>
+    }
+    return (
+        <div>
+            <label>{props.label}</label>
+            {inputElement}
+        </div>
+    )
+}
+
+
+export default input;
