8ecabdeea3a6a57aa33ce4ce1660c3d0f8eb7f87
S. Vincent Parish <vparish13@gmail.com>
2021-03-28 01:36:39 -0500

    created spell logic. seems to be an issue with passing in extended arguments. made several adjustments to character json classes to represent several possible scenarios.

--- src/components/Encounter/ActionModal/ActionModal.tsx
+++ src/components/Encounter/ActionModal/ActionModal.tsx
@@ -28,8 +28,50 @@ class ActionModal extends Component {
         }
     }
     
-    handleSpell(dice : any, target : any) {
-    
+    handleSpell(spell : any, modifiers : any, atkBonus : any, spellSave : any, spellSlots : any, target : any) {
+        let spellType = spell.type;
+        if (spellType == "heal"){
+            let a = 1;
+        }
+        else if (spellType == "help"){
+            let a = 0;
+        }
+        else if (spellType == "attack"){
+            let total = 0;
+            let hit = false;
+            if (spell.attackroll){
+                let attackRoll = Math.floor(Math.random() * 20) + atkBonus;
+                this.setState({attackRollResult: attackRoll})
+                if (attackRoll >= target.armorClass){
+                    hit = true;
+                }
+            }
+            else{
+                let saveModifier = Math.floor((target().modifiers.dex - 10) / 2); // will not always be dex, can find save type in spell. need a switch case helper method to determine which modifier to use
+                let saveRoll = Math.floor(Math.random() * 20) + saveModifier;
+                this.setState({saveRollResult: saveRoll})
+                if (saveRoll >= spellSave){
+                    hit = true;
+                }
+            }
+            if (hit){
+                // play sound of bash, explosion, splash, etc.
+                // this is duplicate code. Any strike can be treated the same way (subtract dice roll from hp)
+                let dice = spell.dice;
+                for (let roll = 0; roll < dice.amount; roll++) {
+                    total += Math.floor(Math.random() * dice.type) + 1;
+                }
+                total += dice.modifier;
+                this.setState({rollResult: total})
+                this.state.target.health -= total;
+            }
+            else{
+                //report roll result? 
+                // play sound of swish 
+                //say "miss" or something
+                this.setState({rollResult: total})
+            }
+        }
     }
 
     selectTarget(combatants : any) {
@@ -113,7 +155,7 @@ class ActionModal extends Component {
                                             attack.dice.modifier
                                         }</div>
                                         <button onClick={
-                                            () => this.handleSpell(attack.dice, this.state.target)
+                                            () => this.handleSpell(this.spell, this.props.combatant.modifiers, this.props.combatant.spellAttackBonus, this.props.combatant.spellSaveDC, this.props.combatant.slots, this.state.target) // pass in "spell" object
                                         }>Roll</button>
                                     </div>
                                 ))
